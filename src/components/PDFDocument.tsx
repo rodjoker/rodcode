96cae5521182bafb041e06bcd4718e9721960c38
RodCode <rodolfoantoniorq@gmail.com>
2025-10-04 20:22:23 +0200

    btn_float_download_cv_ready

--- src/components/PDFDocument.tsx
+++ src/components/PDFDocument.tsx
@@ -1,12 +1,4 @@
-'use client'
-import dynamic from 'next/dynamic';
-import { Document, Page, Text, View, StyleSheet, Font } from '@react-pdf/renderer';
-
-// Dynamically import PDFDownloadLink with no SSR
-const PDFDownloadLink = dynamic(
-  () => import('@react-pdf/renderer').then(mod => mod.PDFDownloadLink),
-  { ssr: false }
-);
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
   page: {
@@ -182,28 +174,6 @@ const MyDocument = () => (
       </View>
     </Page>
   </Document>
-);
-
-export const CVDownloadButton = () => (
-  <PDFDownloadLink document={<MyDocument />} fileName="CV_Rodolfo_Rodriguez.pdf">
-    {({ blob, url, loading, error }) => (
-      <div className="inline-flex items-center px-4 py-2 bg-gray-800 hover:bg-gray-700 text-white rounded-lg transition-colors duration-200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 mr-2"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-        {loading ? 'Cargando...' : 'CV'}
-      </div>
-    )}
-  </PDFDownloadLink>
 );
 
 export default MyDocument;
